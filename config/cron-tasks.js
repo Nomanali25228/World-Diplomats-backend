8a7a63b150c1062610b2d61177b7fae92d5c73d4
Noman Ali <nomi4698dg@gmail.com>
2026-01-17 04:12:50 +0500

    hello g

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -5,7 +5,7 @@ module.exports = {
   // ok
   '*/1 * * * *': async ({ strapi }) => {
     try {
-      const oneHourAgo = new Date(Date.now() + 8 * 60 * 60 * 1000)
+      const oneHourAgo = new Date(Date.now() - 8 * 60 * 60 * 1000)
 
 
 
